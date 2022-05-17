17c1bb91f22b1d72462575d35479b266c7f492e6
Sang Hyun Kim <ksh727301@gmail.com>
2022-05-17 16:45:15 +0900

    N and M (8)

---
+++ Python/15657 - N and M (8).py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort()
+stack = []
+visited = [False for _ in range(n)]
+
+# Back Tracking
+def bt(idx):
+    # print answer
+    if idx == m:
+        for elem in stack:
+            print(elem, end=" ")
+        print("")
+        return
+    # allow overlap and the condition changed
+    for i in range(n):
+        if not visited[i]:
+            stack.append(data[i])
+            bt(idx+1)
+            stack.pop()
+            visited[i] = True
+            for j in range(i+1, n):
+                visited[j] = False
+
+
+bt(0)
