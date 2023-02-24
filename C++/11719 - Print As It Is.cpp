4a48dec2f467a9643ab425b976486de1519ad13b
Sang Hyun (Aaron) Kim <104607302+ilksh@users.noreply.github.com>
2023-02-23 21:39:51 -0500

    11719 - Print As It Is

---
+++ C++/11719 - Print As It Is.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    
+    string s;
+    while(getline(cin, s)) { // get line until program reads nothing
+        cout << s << "\n";  // print as it is
+    }
+    return 0;
+}
