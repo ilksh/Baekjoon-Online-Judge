a567d2f60d5d06ccd184859b81001f555a864843
Sang Hyun Kim <ksh727301@gmail.com>
2024-12-07 23:34:17 -0500

    MCMF

--- C++/3640 - Admiral.cpp
+++ C++/3640 - Admiral.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -6,13 +6,11 @@ typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef pair<int, int> pii;
 
-
-const int MAX = 605;
+const int MAX = 2005;
 const int INF = 1e9;
 
 vvi capacity, flow, cost;
 vvi adj(MAX);
-int V = MAX;
 
 void addEdge(int u, int v, int cap, int w) {
     adj[u].push_back(v);
@@ -22,28 +20,28 @@ void addEdge(int u, int v, int cap, int w) {
     cost[v][u] = -w;
 }
 
-int mcmf (int source, int sink) {
+int mcmf(int source, int sink) {
     int totalCost = 0;
 
-    while(true) {
+    while (true) {
         vi parent(MAX, -1);
         vi dist(MAX, INF);
         queue<int> q;
         vector<bool> inQueue(MAX, false);
-    
+
         dist[source] = 0;
         q.push(source);
         inQueue[source] = true;
 
-        while(!q.empty()) { // find the augmenting path
-            auto cur = q.front(); q.pop();
+        while (!q.empty()) {
+            auto cur = q.front();
+            q.pop();
             inQueue[cur] = false;
 
             for (int next : adj[cur]) {
                 int r = capacity[cur][next] - flow[cur][next];
                 int nextCost = dist[cur] + cost[cur][next];
 
-                // unable to send flow or not a shortest path
                 if (r <= 0 || nextCost >= dist[next]) continue;
 
                 dist[next] = nextCost;
@@ -56,9 +54,8 @@ int mcmf (int source, int sink) {
             }
         }
 
-        // no augmenting path found
         if (parent[sink] == -1) break;
-        
+
         int curFlow = INF;
         for (int p = sink; p != source; p = parent[p]) {
             int par = parent[p];
@@ -73,53 +70,46 @@ int mcmf (int source, int sink) {
             totalCost += curFlow * cost[par][p];
         }
     }
+
     return totalCost;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
-    cin.tie(nullptr); cout.tie(nullptr);
+    cin.tie(nullptr); 
+    cout.tie(nullptr);
 
-    int TC;
-    cin >> TC;
-    
-    while(TC--) {
-        int n, m; // # of node and edge
-        cin >> n >> m;
-        // V = 2 * n + 4;
-
+    int n, m;
+    while (cin >> n >> m) {
         capacity.assign(MAX, vi(MAX, 0));
         flow.assign(MAX, vi(MAX, 0));
         cost.assign(MAX, vi(MAX, INF));
         for (int i = 0; i < MAX; ++i) adj[i].clear();
 
-        int start = 2 * n + 1;
         int source = 2 * n + 2;
         int sink = 2 * n + 3;
 
-        // source-start and set capacity as 2 bc 
-        // There are two groups of tourists planning to visit the city
-        addEdge(source, start, 2, 0);
+        // Source to the first node group
+        addEdge(source, 1 * 2, 2, 0);
 
-        for (int i = 1; i < n + 1; ++i) {
-            // start - node i
-            addEdge(start, i, 1, 0);
-            // in_node - out_node
-            addEdge(i, i + n, 1, -1);
-            // out_node - sink
-            addEdge(i + n, sink, 1, 0);
+        // Sink connection to the last node group
+        addEdge(n * 2, sink, 2, 0);
+
+        // Split each node into in-node and out-node
+        for (int i = 1; i <= n; ++i) {
+            addEdge(i * 2, i * 2 + 1, (i == 1 || i == n) ? 2 : 1, 0);
         }
 
+        // Add connections between nodes
         for (int i = 0; i < m; ++i) {
-            int u, v;
-            cin >> u >> v;
-            // connect node u - node v
-            addEdge(u + n, v, 1, 0);
+            int u, v, w;
+            cin >> u >> v >> w;
+            addEdge(u * 2 + 1, v * 2, 1, w);
         }
 
-        auto rst = mcmf(source, sink);
-
-        cout << -rst << "\n";
+        auto result = mcmf(source, sink);
+        cout << result << "\n";
     }
+
     return 0;
 }
